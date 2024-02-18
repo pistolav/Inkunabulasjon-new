53c9694a84d06876a38bd3bc1fc2159da5f8188c
pistolav <olav.d.eikrem@gmail.com>
2024-02-18 15:50:33 +0100

    Getting there

--- src/sections/simple/SimpleGrid.jsx
+++ src/sections/simple/SimpleGrid.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect} from 'react';
-import samling from "./Samling.json";
+import oversikt from "./Oversikt.json";
 import './SimpleCss.css';
 import Contact from "../../elements/contact/Contact";
 import ImageService from "../../elements/image/imageService";
 import FullImage from "../../elements/image/flipImage";
 
+
+import { useRef } from 'react';
+
+const safeDocument = typeof document !== 'undefined' ? document : {};
 const SimpleGrid = () => {
-    const [collection, setCollection] = React.useState(samling.Publish);
+    const [collection, setCollection] = React.useState(oversikt.web);
     const [show, setShow] = React.useState(false);
     const [element, setElement] = React.useState({
         "Nummer": "0",
@@ -18,15 +22,41 @@ const SimpleGrid = () => {
 
     });
     const [page, setPage] = React.useState(0);
+    const scrollBlocked = useRef();
+    const html = safeDocument.documentElement;
+    const { body } = safeDocument;
+
 
     const showModal = (element, page) => {
         setPage(page);
         setElement(element);
         setShow(true);
+        if (!body || !body.style || scrollBlocked.current) return;
+
+        const scrollBarWidth = window.innerWidth - html.clientWidth;
+        const bodyPaddingRight =
+            parseInt(window.getComputedStyle(body).getPropertyValue("padding-right")) || 0;
+
+        html.style.position = 'relative'; /* [1] */
+        html.style.overflow = 'hidden'; /* [2] */
+        body.style.position = 'relative'; /* [1] */
+        body.style.overflow = 'hidden'; /* [2] */
+        body.style.paddingRight = `${bodyPaddingRight + scrollBarWidth}px`;
+
+        scrollBlocked.current = true;
     }
     const onClose = () => {
         setShow(false);
         setPage(0);
+        if (!body || !body.style || !scrollBlocked.current) return;
+
+        html.style.position = '';
+        html.style.overflow = '';
+        body.style.position = '';
+        body.style.overflow = '';
+        body.style.paddingRight = '';
+
+        scrollBlocked.current = false;
     }
     const onNext = () => {
         console.log("Next");
@@ -49,7 +79,7 @@ const SimpleGrid = () => {
 
 
     return (
-        <div className="grid-container-vertical">
+        <div className="grid-container-vertical" style={show?{}:{overflow:"hidden"}}>
             {
                 collection.map((row, i) => (
                     <Item key={i} index={i} props={row} showModal={showModal}/>
@@ -71,8 +101,8 @@ const Item = props => {
             }
         }>
             <ImageService imageName={props.props["Nummer"]}/>
-            <h3>Title: {props.props["Nummer"]}</h3>
-            <p>Sub</p>
+            <h3>{props.props["Nummer"]}</h3>
+            <p>{props.props["År"]}</p>
         </div>
     );
 
@@ -80,6 +110,13 @@ const Item = props => {
 
 
 const Modal = props => {
+    const [visibleField, setVisibleField] = React.useState("image");
+
+    const onChangeVisibleField = (field) => {
+        setVisibleField(field);
+        console.log("VisibleField", visibleField);
+    }
+
     if (!props.show || props.element === null || props.element === undefined) {
         return null;
     }
@@ -104,11 +141,11 @@ const Modal = props => {
                         }
                     }>&#8249;</button>
 
-                    <div className= "grid-modal-body-left content">
+                    <div className={(visibleField==="image"?(""):("hide-mobile")) + " grid-modal-body-left content"} >
                         <FullImage imageName={props.element.Nummer}/>
                     </div>
-                    <div className="grid-modal-body-right content">
-                        <RightContent element={props.element}/>
+                    <div className={(visibleField!=="image"?(""):("hide-mobile")) + " grid-modal-body-right content"}>
+                        <RightContent element={props.element} display={visibleField}/>
                     </div>
                     <button className="next nav-button grid-modal-button modal-button" onClick={
                         (e) => {
@@ -119,9 +156,9 @@ const Modal = props => {
                     }>&#8250;</button>
                 </div>
                 <div className="grid-modal-footer">
-                    <button className="grid-modal-button modal-button-info-switch">Se bilde</button>
-                    <button className="grid-modal-button modal-button-info-switch">Se info</button>
-                    <button className="grid-modal-button modal-button-info-switch">Ta kontakt</button>
+                    <button className="grid-modal-button modal-button-info-switch" onClick={()=>onChangeVisibleField("image")}>Se bilde</button>
+                    <button className="grid-modal-button modal-button-info-switch" onClick={() => onChangeVisibleField("info")}>Se info</button>
+                    <button className="grid-modal-button modal-button-info-switch" onClick={() => onChangeVisibleField("contact")}>Ta kontakt</button>
 
                 </div>
             </div>
@@ -144,33 +181,48 @@ const salgsstatus = (solgt) => {
 
 const RightContent = (props) => {
     const [displayInfo, setDisplayInfo] = React.useState(true);
-    console.log("RightContent", props);
 
     return (
         <div className="grid-modal-right-content">
             <div className="grid-modal-right-content-body">
-                <div className="info" style={{display: displayInfo?(""):("none")}}>
+                <div className={props.display==="info"?(""):("hide-mobile") +" info"} style={{display: displayInfo?(""):("none")}}>
                     <h3>Info</h3>
                     <div className="inline">
                         <p>Nummer:</p>
                         <p>{props.element.Nummer}</p>
                     </div>
                     <div className="inline">
-                        <p>År:</p>
-                        <p>{props.element.Bakside}</p>
+                        <p>Bokas navn:</p>
+                        <p>{props.element["Bokas navn"]}</p>
                     </div>
                     <div className="inline">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium assumenda, blanditiis dicta eveniet facere fugiat incidunt itaque iure, mollitia nam natus nobis numquam odio reiciendis similique soluta sunt voluptas!</p>
+                        <p>Trykket i:</p>
+                        <p>{props.element["Trykket i"]}</p>
+                    </div>
+                    <div className="inline">
+                        <p>Trykket av:</p>
+                        <p>{props.element["Trykket av"]}</p>
+                    </div>
+                    <div className="inline">
+                        <p>År:</p>
+                        <p>{props.element.År}</p>
+                    </div>
+                    <div className="inline">
+                        <p>Solgt:</p>
+                        <p>{props.element["Solgt"]!==0 ? ("Nei"):("Ja")}</p>
+                    </div>
+                    <div className="inline">
+                        <p>{props.element["Beskrivelse"]}</p>
                     </div>
                 </div>
-                <div style={{display: displayInfo?("none"):("")}}>
+                <div className={props.display==="contact"?(""):("hide-mobile")} style={{display: displayInfo && props.display!=="contact"?("none"):("")}}>
                     <h3>Ta kontakt</h3>
                     <Contact className="grid-modal-contact" element={props.element}/>
                 </div>
             </div>
 
             <div className="grid-modal-right-content-info-footer">
-                <button className="grid-modal-button modal-button-info-switch" onClick={
+                <button className="grid-modal-button modal-button-info-switch hide-mobile" onClick={
                     (e) => {
                         e.preventDefault();
                         setDisplayInfo(!displayInfo);
